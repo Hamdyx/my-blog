a7afba67e372be0d499585062b312409d07daeeb
hamdyx <a.hamdy.nig@gmail.com>
2022-04-09 13:01:37 +0200

    REFACTOR: navar migration to bootstrap

--- src/layout/components/navbar/index.tsx
+++ src/layout/components/navbar/index.tsx
@@ -1,37 +1,42 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
 const NavBar: React.FC = (props) => {
 	const [activePath, setActivePath] = useState(window.location.pathname);
-	const pathNames = ['home', 'about'];
 
 	const handleActive = (e: any) => {
 		setActivePath(e.currentTarget.pathname);
 	};
 
 	return (
-		<nav>
-			<ul>
-				<li>
-					<Link
-						to="/"
-						className={`${activePath === '/' ? 'active' : ''}`}
-						onClick={handleActive}
-					>
-						Home
-					</Link>
-				</li>
-				<li>
-					<Link
-						to="/about"
-						className={`${activePath === '/about' ? 'active' : ''}`}
-						onClick={handleActive}
-					>
-						Course 1
-					</Link>
-				</li>
-			</ul>
-		</nav>
+		<Navbar bg="dark" variant="dark" expand="lg">
+			<Container>
+				<Link to="/" className={`navbar-brand`}>
+					Hamdyx-Blog
+				</Link>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Link
+							to="/"
+							className={`${activePath === '/' ? 'active' : ''} nav-link`}
+							onClick={handleActive}
+						>
+							Home
+						</Link>
+						<Link
+							to="/about"
+							className={`${activePath === '/about' ? 'active' : ''} nav-link`}
+							onClick={handleActive}
+						>
+							About
+						</Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
 	);
 };
 
