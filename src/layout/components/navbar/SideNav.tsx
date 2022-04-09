8940a99023d1b53cee9378ec40c261252be1eb39
hamdyx <a.hamdy.nig@gmail.com>
2022-04-09 12:28:02 +0200

    ADD: navbar active styles

--- src/layout/components/navbar/SideNav.tsx
+++ src/layout/components/navbar/SideNav.tsx
@@ -8,7 +8,7 @@ const SideNav: React.FC = (props) => {
 		`javascript to the rescue`,
 		`react vs vanilla javascript`,
 		`building single-page-applications (SPA)`,
-		'alternative-frameworks',
+		'alternative frameworks',
 	];
 
 	const menu_2 = [
--- src/layout/components/navbar/index.tsx
+++ src/layout/components/navbar/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = (props) => {
+	const [activePath, setActivePath] = useState(window.location.pathname);
+	const pathNames = ['home', 'about'];
+
+	const handleActive = (e: any) => {
+		setActivePath(e.currentTarget.pathname);
+	};
+
 	return (
 		<nav>
 			<ul>
 				<li>
-					<Link to="/">Home</Link>
+					<Link
+						to="/"
+						className={`${activePath === '/' ? 'active' : ''}`}
+						onClick={handleActive}
+					>
+						Home
+					</Link>
 				</li>
 				<li>
-					<Link to="/about">Course 1</Link>
+					<Link
+						to="/about"
+						className={`${activePath === '/about' ? 'active' : ''}`}
+						onClick={handleActive}
+					>
+						Course 1
+					</Link>
 				</li>
 			</ul>
 		</nav>
